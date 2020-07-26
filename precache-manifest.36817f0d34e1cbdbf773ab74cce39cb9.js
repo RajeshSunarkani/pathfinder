54f01e5b1d243549d7b4a59e528a2e8ae6880f12
RajeshSUnarkani <sunarkani.rajesh.phy16@itbhu.ac.in>
2020-07-26 19:59:20 +0530

    Updates

---
+++ precache-manifest.36817f0d34e1cbdbf773ab74cce39cb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef53b76d0b270d0fa2c4e8a4adecbc8a",
+    "url": "/pathfinder/index.html"
+  },
+  {
+    "revision": "d811b0e8e8b20f569988",
+    "url": "/pathfinder/static/css/main.0635c281.chunk.css"
+  },
+  {
+    "revision": "04542d8c4ea8d516d156",
+    "url": "/pathfinder/static/js/2.ef54b007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pathfinder/static/js/2.ef54b007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d811b0e8e8b20f569988",
+    "url": "/pathfinder/static/js/main.7121947a.chunk.js"
+  },
+  {
+    "revision": "a2e0f146f336e32039c6",
+    "url": "/pathfinder/static/js/runtime-main.f2fbd7ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pathfinder/static/media/logo.5d5d9eef.svg"
+  }
+]);
